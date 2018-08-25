305d0bb25952f2e6d3369fc73266fa258aa3df70
frank-guo <frank_guoguo@hotmail.com>
2018-08-24 22:20:57 -0700

    Migration files for changing Weight and Measurement to string

---
+++ EShipment/Data/Migrations/20180825051307_ChangeFloatToStringINOrder.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EShipment.Data.Migrations
+{
+    public partial class ChangeFloatToStringINOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Weight",
+                table: "Orders",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Measurement",
+                table: "Orders",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Weight",
+                table: "Orders",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Measurement",
+                table: "Orders",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
